93ade9d508b7cbd2a3950ab4bd8370196f596e12
MarceloR <mruilova042@puce.edu.ec>
2020-10-11 08:52:29 -0500

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -12,9 +12,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
+exports.getToken = function (user) {
     return jwt.sign(user, config.secretKey,
-        {expiresIn: 3600});
+        { expiresIn: 3600 });
 };
 
 var opts = {};//options
@@ -24,7 +24,7 @@ opts.secretOrKey = config.secretKey;
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
         console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
             if (err) {
                 return done(err, false);
             }
@@ -37,4 +37,13 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser =
+exports.verifyAdmin = function (req, res, next) {
+    if (req.user.admin) {
+        next();
+    } else {
+        var err = new Error('You are not authorized to perform this operation');
+        err.status = 403;
+        next(err);
+    }
+}
