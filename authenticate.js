ee16d6391247fa0e1e0cde6e5617f1edec5910d2
MarceloR <mruilova042@puce.edu.ec>
2020-10-13 09:26:52 -0500

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,7 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
+//var FacebookTokenStrategy = require('passport-facebook-token');
 
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -46,4 +47,30 @@ exports.verifyAdmin = function (req, res, next) {
         err.status = 403;
         next(err);
     }
+}/*
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
 }
+));*/
--- config.js
+++ config.js
@@ -2,3 +2,11 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion'
 }
+/*module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 'Your Client App ID',
+        clientSecret: 'Your Client App Secret'
+    }
+}*/
